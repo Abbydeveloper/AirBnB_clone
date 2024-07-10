6d14f46807e28fbe311846d7ed3a3e6978b13009
Adeboga Abigail <abbyboga@gmail.com>
2024-07-10 05:06:34 -0700

    Add Base Model class

---
+++ models/base_model.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Base Model class module"""
+
+
+import models
+import uuid
+import datetime
+
+time_format = "%Y-%m-%dT%H:%M:%S.%f"
+
+class BaseModel():
+    """BaseModel class"""
+
+    def __init__(self):
+        """Initialize class"""
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        models.storage.new(self)
+
+    def save(self):
+        """update the public instance attribute updated_at with the current
+        datetime"""
+        self.update_at = datetime.now()
+        models.storage.save()
+
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the
+        instance"""
+        dict_obj = self.__dict__.copy()
+        dict_obj["__class__"] = type(self).__name__
+        dict_obj["created_at"] = self.created_at.strftime(time_format)
+        dict_obj["updated_at"] = self.updated_at.strftime(time_format)
+        return dict_obj
