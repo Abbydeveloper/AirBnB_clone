e087523406e1bcd0cf269336bd40018035f0d397
Adeboga Abigail <abbyboga@gmail.com>
2024-07-10 07:31:45 -0700

    Fix error

--- models/base_model.py
+++ models/base_model.py
@@ -18,12 +18,6 @@ class BaseModel():
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
-    def save(self):
-        """update the public instance attribute updated_at with the current
-        datetime"""
-        self.updated_at = datetime.now()
-
-
     def to_dict(self):
         """returns a dictionary containing all keys/values of __dict__ of the
         instance"""
