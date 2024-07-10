05ebf1a40862bc56b61274e3e72213e3eda72516
Adeboga Abigail <abbyboga@gmail.com>
2024-07-10 07:00:12 -0700

    Add Base Model class

--- models/base_model.py
+++ models/base_model.py
@@ -17,13 +17,11 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
-        """models.storage.new(self)"""
 
     def save(self):
         """update the public instance attribute updated_at with the current
         datetime"""
         self.update_at = datetime.now()
-        """models.storage.save()"""
 
 
     def to_dict(self):
