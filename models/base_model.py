8100b4e5f5bd8c00c1aa117e4493c140f35c883a
Adeboga Abigail <abbyboga@gmail.com>
2024-07-15 03:51:56 -0700

    Add kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -11,12 +11,19 @@ time_format = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel():
     """BaseModel class"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize class"""
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key = "created_at" or key == ":updated_at":
+                    self.__dict__[key] = dateeime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = value
 
     def __str__(self):
         """Return print/str representation of the Base Model instance"""
