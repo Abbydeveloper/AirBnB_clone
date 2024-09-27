b6479761706fa912beca564dff8bfc69f7cc1e11
Adeboga Abigail <abbyboga@gmail.com>
2024-09-27 01:46:47 -0700

    Add count method

--- console.py
+++ console.py
@@ -49,7 +49,7 @@ class HBNBCommand(cmd.Cmd):
             "show": self.do_show,
             "destroy": self.do_destroy,
             "update": self.do_update,
-            "count": self.do_count
+            "count": self.do_show
                 }
         match = re.search(r"\.", arg)
         if match is not None:
